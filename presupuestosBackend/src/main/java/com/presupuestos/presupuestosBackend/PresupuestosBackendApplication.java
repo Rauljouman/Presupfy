502915f4b825e001af62ff42534e698c3284972a
Raul Ip <rauljouman@gmail.com>
2026-08-20 12:18:40 +0200

    Proyecto iniciado

---
+++ presupuestosBackend/src/main/java/com/presupuestos/presupuestosBackend/PresupuestosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.presupuestos.presupuestosBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresupuestosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresupuestosBackendApplication.class, args);
+	}
+
+}
---
+++ presupuestosBackend/src/test/java/com/presupuestos/presupuestosBackend/PresupuestosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.presupuestos.presupuestosBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresupuestosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
